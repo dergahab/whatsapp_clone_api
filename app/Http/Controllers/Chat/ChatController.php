b38498ffd7303f2b48564ccee293fe65eeaac7c7
@c.mhatzadeh <user0@erp-intel.local>
2025-04-08 18:06:16 +0400

    feet:create search

--- app/Http/Controllers/Chat/ChatController.php
+++ app/Http/Controllers/Chat/ChatController.php
@@ -6,6 +6,7 @@ use App\Events\Message;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Chat\ChatStoreRequest;
 use App\Http\Requests\Chat\DestroyRequest;
+use App\Http\Requests\Chat\SearcRequest;
 use App\Http\Requests\Chat\ShowRequest;
 use App\Services\Chat\ChatService;
 use Illuminate\Http\JsonResponse;
@@ -17,7 +18,7 @@ class ChatController extends Controller
 {
     public function __construct(public ChatService $service) {}
 
-    public function index(Request $request)
+    public function index(SearcRequest $request)
     {
         DB::beginTransaction();
         try {
