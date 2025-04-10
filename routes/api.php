9fd6e421c03251c323f43ac915a8862e99624c86
@c.mhatzadeh <user0@erp-intel.local>
2025-04-10 18:26:21 +0400

    feet: create add user

--- routes/api.php
+++ routes/api.php
@@ -54,4 +54,8 @@ Route::middleware(['auth:sanctum'])->group(function () {
     Route::apiResource('messages', MessageController::class)->except(['index'])->parameters(['messages' => 'uuid']);
     Route::apiResource('groups', GroupController::class)->parameters(['groups' => 'uuid']);
     Route::get('/chat/messages', [MessageController::class, 'show_messages']);
+    Route::post('addUser', [GroupController::class, 'addUser']);
+
+
+
 });
