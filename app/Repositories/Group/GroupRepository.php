e1188badd278f2b39e99501b5c0ec2c494ef92a0
dargah.ah <dergah@>
2025-05-01 12:51:05 +0400

    fix: side bar

--- app/Repositories/Group/GroupRepository.php
+++ app/Repositories/Group/GroupRepository.php
@@ -17,6 +17,9 @@ class GroupRepository
                 $q->where('name', 'like', '%'.$search.'%');
             })
             ->withCount('unread_messages')
+	        ->whereHas('usres', function ($q) {
+	            $q->where('uuid', auth()->user()->uuid);
+	        })
             ->get();
     }
 
